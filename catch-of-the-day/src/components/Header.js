d217ab98939cea272b0e9678e10bd8f9eb571871
Roger Hwang <hroger19@gmail.com>
2019-03-01 18:10:26 -0800

    created the 3 main components: menu, order, inventory

---
+++ catch-of-the-day/src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+class Header extends React.Component {
+    render () {
+        return (
+            <React.Fragment>
+                <header className="top">
+                    <h1>Catch 
+                        <span className="ofThe">
+                            <span className="of">Of</span>
+                            <span className="the">The</span>
+                        </span>
+                    Day</h1>
+                </header>
+                <h3 className="tagline">
+                    <span>Fresh Daily</span>
+                </h3>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Header;
