6b8b6205b6d137518dbad15d8da57e32ead80f1d
Roger Hwang <hroger19@gmail.com>
2019-03-01 21:42:20 -0800

    added props to Header component

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -3,19 +3,17 @@ import React from "react";
 class Header extends React.Component {
     render () {
         return (
-            <React.Fragment>
-                <header className="top">
-                    <h1>Catch 
-                        <span className="ofThe">
-                            <span className="of">Of</span>
-                            <span className="the">The</span>
-                        </span>
-                    Day</h1>
-                </header>
+            <header className="top">
+                <h1>Catch 
+                    <span className="ofThe">
+                        <span className="of">Of</span>
+                        <span className="the">The</span>
+                    </span>
+                Day</h1>
                 <h3 className="tagline">
-                    <span>Fresh Daily</span>
+                    <span>{this.props.tagline}</span>
                 </h3>
-            </React.Fragment>
+            </header>
         );
     }
 }
