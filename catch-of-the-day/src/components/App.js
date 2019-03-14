f3c13c8cb0bf0aa2178e73a4cbdc58741495e9aa
Roger Hwang <hroger19@gmail.com>
2019-03-13 17:58:32 -0700

    display inventory orders to order component

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -5,6 +5,7 @@ import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
 import sampleFishes from "../sample-fishes";    //can rename it since we're importing default
+import Fish from "./Fish";   
 
 class App extends Component {
     state = {
@@ -29,13 +30,27 @@ class App extends Component {
         });
     }
 
+    addToOrder = (key) => {
+        //1. Take a copy of state
+        const order = {...this.state.order};
+        //2. Either add to the order, or update the number in our order
+        order[key] = order[key] + 1 || 1; 
+        //3. Call setState to update our state object
+        this.setState({
+            order: order    //or just 'order'
+        });
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market" />
+                    <ul className="fishes">
+                        {Object.keys(this.state.fishes).map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)}
+                    </ul>
                 </div>
-                <Order />
+                <Order fishes={this.state.fishes} order={this.state.order} />   {/* can also do {...this.state} but not as modular */}
                 <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />  {/*add prop with same name as property of method- goal is to update state from form*/}
             </div>
         );
