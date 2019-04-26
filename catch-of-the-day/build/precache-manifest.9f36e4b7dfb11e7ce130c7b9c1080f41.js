f08d0131594ceb43c27fedd3ee8fa5c91f7c89e5
Roger Hwang <hroger19@gmail.com>
2019-04-25 23:54:53 -0700

    deploy app to Netlify @ https://5cc2aac7f81a0d0a18610314--quizzical-chandrasekhar-4b60dc.netlify.com

---
+++ catch-of-the-day/build/precache-manifest.9f36e4b7dfb11e7ce130c7b9c1080f41.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "afc3c0b73dd9c15de602",
+    "url": "/static/css/main.712d47f8.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f9364e7cc0030015aeb3",
+    "url": "/static/js/2.c5699358.chunk.js"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/static/media/haymaker-webfont.e9535f20.ttf"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "afc3c0b73dd9c15de602",
+    "url": "/static/js/main.b17d7fcd.chunk.js"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "47e26dee9b053b540478750bdf7a87d5",
+    "url": "/static/media/haymaker-webfont.47e26dee.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "c175fe49ca6c599199a81c6791490344",
+    "url": "/static/media/blanch_caps_inline-webfont.c175fe49.svg"
+  },
+  {
+    "revision": "6ac4c674ccfe82f49c8efc8589c1a16e",
+    "url": "/index.html"
+  }
+];
